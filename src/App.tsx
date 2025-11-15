25e23fa355a8a70a6e5361b0360d462052eb92dd
Samir <samir.lazzali@gmail.com>
2025-11-15 20:05:12 +0100

    redirect 404 to index

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import Index from "./pages/Index";
@@ -39,9 +39,8 @@ const App = () => (
               <Route path="/modeles/:id" element={<ModeleDetail />} />
               <Route path="/soumission/:id" element={<SoumissionDetail />} />
               <Route path="/merci" element={<Merci />} />
-
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
+              <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
           </main>
           <Footer />
